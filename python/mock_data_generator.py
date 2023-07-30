409325e75ae3fb90c3e1e39ffdfba3e2bf27a86a
Gokuldev-PS <132561683+Gokuldev-PS@users.noreply.github.com>
2023-07-30 16:29:58 +0530

    Create mock_data_generator.py

---
+++ python/mock_data_generator.py
@@ -0,0 +1,48 @@
+import time
+import random
+import json
+from confluent_kafka import Producer
+# Configuration for Confluent Cloud
+BOOTSTRAP_SERVERS = ''  # Replace with your Confluent Cloud bootstrap servers
+SECURITY_PROTOCOL = 'SASL_SSL'
+SASL_MECHANISM = 'PLAIN'
+SASL_USERNAME = ''  # Replace with your Confluent Cloud API key
+SASL_PASSWORD = ''  # Replace with your Confluent Cloud API secret
+TOPIC_NAME = 'telemetry_events'  
+def generate_mock_data():
+    user_ids = ['user1', 'user2', 'user3', 'user4', 'user5','user6']
+    event_types = ['kill', 'death']
+    current_time = int(time.time())  # Get the current UNIX timestamp
+    data= {
+        'userid': random.choice(user_ids),
+        'eventtype': random.choice(event_types),
+        'time': current_time
+    }
+    return json.dumps(data)
+def delivery_report(err, msg):
+    if err is not None:
+        print(f"Message delivery failed: {err}")
+    else:
+        print(f"Message delivered to {msg.topic()} [{msg.partition()}] at offset {msg.offset()}")
+def main():
+    conf = {
+        'bootstrap.servers': BOOTSTRAP_SERVERS,
+        'security.protocol': SECURITY_PROTOCOL,
+        'sasl.mechanisms': SASL_MECHANISM,
+        'sasl.username': SASL_USERNAME,
+        'sasl.password': SASL_PASSWORD,
+    }
+    producer = Producer(conf)
+    try:
+        while True:
+            mock_data = generate_mock_data()
+          #  message_value = f"{mock_data['userid']},{mock_data['eventtype']},{mock_data['time']}"
+            producer.produce(TOPIC_NAME, mock_data.encode('utf-8'), callback=delivery_report)
+            producer.flush()
+            time.sleep(1)  # Send a new message every 1 second
+    except KeyboardInterrupt:
+        print("User interrupted, stopping data generation.")
+    finally:
+        producer.flush()
+if __name__ == "__main__":
+    main()
