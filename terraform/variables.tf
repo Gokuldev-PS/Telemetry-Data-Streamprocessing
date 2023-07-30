e3cd4532d29de6d952aabef8fe3ddb2404039e72
Gokuldev-PS <132561683+Gokuldev-PS@users.noreply.github.com>
2023-07-30 12:58:09 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,12 +1,12 @@
 variable "confluent_cloud_api_key" {
   description = "Confluent Cloud API Key (also referred as Cloud API ID)"
   type        = string
-  default = "ZKPDQLF2POQEEAOT"
+  default = " "   #Add your API Key created during pre-requsite
 }
 
 variable "confluent_cloud_api_secret" {
   description = "Confluent Cloud API Secret"
   type        = string
   sensitive   = true
-  default = "7zUeI/Zuj6rfiw6vI1Zs474JRamT4ynnd7i498cA1m5pWTWsPBayG9SQvNjRLYLO"
+  default = " "    #Add your API secret created during pre-requsite
 }
